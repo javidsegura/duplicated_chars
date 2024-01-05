001943512f0d49f0f411f6ffe91ebd089f9817ca
Javi Segura <129964070+javidsegura@users.noreply.github.com>
2024-01-05 14:26:24 +0000

    Add files via upload

---
+++ try1.py
@@ -0,0 +1,29 @@
+""" I am trying to create a function that identifies the characthers that are being repeated (>2)
+in a string, and return the characther and the amount of times it has been. """
+
+
+string = input("Send your string here: ")
+string = string.replace(" ", "")
+
+l = []
+sequence = []
+
+for i in string:
+      if i in l:
+            sequence.append(i)
+
+      else:
+            l.append(i) 
+
+results = set()
+def func(sequence):
+      for i in sequence:
+            r = sequence.count(i)
+            if r > 1:
+                  results.add(f"'{i}' appears {r + 1} times")
+            else:
+                  results.add(f"'{i}' appears twice")
+      return results
+            
+print(func(sequence))
+
