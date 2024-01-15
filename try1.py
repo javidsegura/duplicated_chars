bf1267590e9c7041a0bef96e61635d08cb7f438e
Javi Segura <129964070+javidsegura@users.noreply.github.com>
2024-01-15 17:46:32 +0100

    Comments try1.py

--- try1.py
+++ try1.py
@@ -1,26 +1,24 @@
 """ I am trying to create a function that identifies the characthers that are being repeated (>2)
 in a string, and return the characther and the amount of times it has been. """
 
-#Get the string and eliminate the spaces 
 string = input("Send your string here: ")
-string = string.replace(" ", "")
+string = string.replace(" ", "") #Get the string and eliminate the spaces 
 
-#
-l = []
 sequence = []
+duplicated = [] 
+results = set() # Make sure results are not repeated for every duplicated i
 
 for i in string:
-      if i in l:
-            sequence.append(i)
+      if i in duplicated: #Originally duplicated starts empty
+            sequence.append(i)  # Add non-duplicated chars 
       else:
-            l.append(i) 
+            duplicated.append(i) # Add duplicated chars
 
-results = set()
 def func(sequence):
-      for i in sequence:
-            r = sequence.count(i)
+      for i in sequence: #Check within the duplicated list
+            r = sequence.count(i) #Count how many times a single character appears
             if r > 1:
-                  results.add(f"'{i}' appears {r + 1} times")
+                  results.add(f"'{i}' appears {r + 1} times") 
             else:
                   results.add(f"'{i}' appears twice")
       return results
